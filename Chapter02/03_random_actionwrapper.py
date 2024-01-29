322a619416a88a77b8fc82ab07952808ec27a228
xiangyuejia <xiangyuejia@qq.com>
2024-01-29 15:35:43 +0800

    fix: chapter02 gymnasium

--- Chapter02/03_random_actionwrapper.py
+++ Chapter02/03_random_actionwrapper.py
@@ -1,4 +1,5 @@
-import gym
+import gymnasium as gym
+
 
 import random
 
@@ -16,13 +17,13 @@ class RandomActionWrapper(gym.ActionWrapper):
 
 
 if __name__ == "__main__":
-    env = RandomActionWrapper(gym.make("CartPole-v0"))
+    env = RandomActionWrapper(gym.make("CartPole-v1", render_mode="human"))
 
     obs = env.reset()
     total_reward = 0.0
 
     while True:
-        obs, reward, done, _ = env.step(0)
+        obs, reward, done, _, _ = env.step(0)
         total_reward += reward
         if done:
             break
--- Chapter02/04_cartpole_random_monitor.py
+++ Chapter02/04_cartpole_random_monitor.py
@@ -1,9 +1,12 @@
-import gym
+from aitool import make_dir
+import gymnasium as gym
+from gymnasium.wrappers.monitoring.video_recorder import VideoRecorder
 
 
 if __name__ == "__main__":
-    env = gym.make("CartPole-v0")
-    env = gym.wrappers.Monitor(env, "recording")
+    env = gym.make("CartPole-v1", render_mode="rgb_array")
+    make_dir('./recorder')
+    video_recorder = VideoRecorder(env, path='./recorder/tmp.mp4')
 
     total_reward = 0.0
     total_steps = 0
@@ -11,12 +14,15 @@ if __name__ == "__main__":
 
     while True:
         action = env.action_space.sample()
-        obs, reward, done, _ = env.step(action)
+        video_recorder.capture_frame()
+        obs, reward, done, _, _ = env.step(action)
+        print(obs)
         total_reward += reward
         total_steps += 1
         if done:
             break
 
     print("Episode done in %d steps, total reward %.2f" % (total_steps, total_reward))
+    video_recorder.close()
+    video_recorder.enabled = False
     env.close()
-    env.env.close()
